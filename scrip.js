eded12912f420657d221cdcdbaec5332223bdde0
nthanh <nthanh.hcp@gmail.com>
2022-04-24 10:29:03 +0700

    first commit

---
+++ scrip.js
@@ -0,0 +1,11 @@
+const btn = document.querySelectorAll("button")
+btn.forEach(function(button,index){
+button.addEventListener("click",function(event){{
+    var btnItem = event.target
+    var product = btnItem.parentElement
+    var productImg = product.querySelector(img).src 
+    var productName = product.querySelector("")
+    console.log(btn)
+}})
+
+})
